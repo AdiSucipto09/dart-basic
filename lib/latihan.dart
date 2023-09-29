fa10241ecfebdc6afd81609269dc09a407fe6022
AdiSucipto29 <suciptoadi1199@gmail.com>
2023-09-29 17:16:47 +0700

    first commit

---
+++ lib/latihan.dart
@@ -0,0 +1,9 @@
+// import 'dart:io';
+
+// void main(){
+//   print("Masukkan Alas dan Tinggi");
+
+//   String input = stdin.readLineSync()!;
+//   double alas = double.parse(input);
+//   double tinggi = double.parse(input);
+// }
